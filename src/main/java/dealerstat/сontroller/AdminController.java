7109520c3c83be176f38bc74a4c67ed41594a1e1
BatukIlya <batukilya171213@mail.ru>
2021-12-05 22:17:59 +0300

    Done

--- src/main/java/dealerstat/сontroller/AdminController.java
+++ src/main/java/dealerstat/сontroller/AdminController.java
@@ -34,5 +34,6 @@ public class AdminController {
     @PutMapping("/comment_request/{commentId}/")
     public ResponseEntity<?> approveComment(@PathVariable Long commentId) {
         return adminService.approveComment(commentId);
+
     }
 }
